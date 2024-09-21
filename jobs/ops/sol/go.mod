a43176fda50b6b93d8c782825b510cffa22d9bf3
timotewb <timote.wb@gmail.com>
2024-09-21 13:24:28 +1200

    sol dev

---
+++ jobs/ops/sol/go.mod
@@ -0,0 +1,3 @@
+module github.com/timotewb/cpu/jobs/ops/sol
+
+go 1.21.4
---
+++ jobs/ops/sol/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main(){
+	var ipAddress string
+	var userName string
+	var help bool
+	flag.StringVar(&ipAddress, "i", "", "IP Address of target to shutdown (shorthand)")
+	flag.StringVar(&ipAddress, "ip-address", "", "IP Address of target to shutdown")
+	flag.StringVar(&userName, "u", "", "User name who will shutdown target (shorthand)")
+	flag.StringVar(&userName, "user-name", "", "User name who will shutdown target")
+	flag.BoolVar(&help, "h", false, "Show usage instructions (shorthand)")
+	flag.BoolVar(&help, "help", false, "Show usage instructions")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "----------------------------------------------------------------------------------------")
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Pass -i to specify IP Address of target to shutdown (required)")
+		fmt.Fprintln(os.Stderr, "  -i\t\tstring\n  --ip-address")
+		fmt.Fprintln(os.Stderr, "Pass -u to specify User name who will shutdown target (required)")
+		fmt.Fprintln(os.Stderr, "  -u\t\tstring\n  --user-name")
+		fmt.Fprintln(os.Stderr, "Pass -h to show usage instructions")
+		fmt.Fprintln(os.Stderr, "  -h\t\tstring\n  --help")
+		fmt.Fprintln(os.Stderr, "----------------------------------------------------------------------------------------")
+	}
+	flag.Parse()
+
+	// Print the Help docuemntation to the terminal if user passes help flag
+	if help {
+		flag.Usage()
+		return
+	}
+
+	// check required flags
+	if ipAddress == "" || userName == "" {
+		log.Fatal("from sol(): required parameters cannot be blank")
+		return
+	}
+
+	// Execute the command
+	cmd := exec.Command("ssh", fmt.Sprintf("%q@%q", userName, ipAddress), "sudo", "shutdown", "now")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("from sol(): function exec.Command() failed: %v", err)
+		return
+	}
+	log.Printf("from sol(): %v", output)
+}
